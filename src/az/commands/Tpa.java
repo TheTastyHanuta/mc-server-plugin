75ccad780bebe847c9f2f527d48b5a9eb1a72925
TheTastyHanuta <luis.kriegermc@gmail.com>
2022-03-18 15:24:50 +0100

    Added /sleep and new tpa features plus some small fixes

--- src/az/commands/Tpa.java
+++ src/az/commands/Tpa.java
@@ -1,6 +1,5 @@
 package az.commands;
 
-
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -17,26 +16,26 @@ public class Tpa implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
 	{
+		if(sender instanceof Player) {
+			
 		Player p = (Player)sender;
 		Player p2 = Bukkit.getPlayer(args[0]);
 		
 		if(args.length == 1) 
 		{
 			if(p == p2) {
-				p.sendMessage(Mainclass.prefix + "§cDu kannst dir nicht selber eine Anfrage schicken!");
-				
+				p.sendMessage(Mainclass.prefix + "§cDu kannst dir nicht selber eine Anfrage schicken!");	
+			}else if(p2 == null){
+				p.sendMessage(Mainclass.prefix + "§cDer angegebene Spieler ist nicht online!");
 			}else {
-			try 
-			{
-				
-				
+					Player p3 = Mainclass.tpa.get(p2);
+					
+					if(p3 != p) {						
 				Mainclass.tpa.put(p2, p);
-				
-			{
-				 	
-			p2.sendMessage(Mainclass.prefix + p.getName() + "§a möchte sich zu dir teleportieren! Benutze §c/tpaccept §aum die Anfrage anzunehmen oder §c/tpadeny §aum sie abzulehnen! Du hast 5 Minuten Zeit!");
+			 	
+			p2.sendMessage(Mainclass.prefix + p.getName() + "§a möchte sich zu dir teleportieren! Benutze §c/tpaaccept §aum die Anfrage anzunehmen oder §c/tpadeny §aum sie abzulehnen! Du hast 5 Minuten Zeit!");
 			
-			p.sendMessage(Mainclass.prefix + "§a" + p2.getName() + " hat die Anfrage bekommen!");
+			p.sendMessage(Mainclass.prefix + "§a" + p2.getName() + " hat die Anfrage bekommen! Nutze §c/tpacancel §8<Spielername>§a um sie abzubrechen!");
 			
 			new java.util.Timer().schedule(
 					new java.util.TimerTask() {
@@ -53,18 +52,16 @@ public class Tpa implements CommandExecutor {
 					},
 					300000
 				);
+			}else {
+				p.sendMessage(Mainclass.prefix + "§cDu hast bereits eine Anfrage verschickt! Nutze §a/tpacancel <Spielername>§c um sie abzubrechen!");
+				return false;
 			}
 			}
-			catch(Exception ex) 
-			{
-				p.sendMessage(Mainclass.prefix + "§cDiese Person ist nicht online!");
-			}
-			}
-			
 		}
 		else
 		{
 			p.sendMessage(Mainclass.prefix + "§cBitte benutze /tpa <SPIELER>!");
+		}
 		}
 		return false;
 	}
--- src/az/commands/TpaAccept.java
+++ src/az/commands/TpaAccept.java
@@ -1,17 +1,30 @@
 package az.commands;
 
+import java.util.HashMap;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 import az.main.Mainclass;
 
 public class TpaAccept implements CommandExecutor{
 	
+	private HashMap<Player, Location> lastLoc = new HashMap<>(); 
+	private HashMap<Player, Location> lastLoc2 = new HashMap<>(); 
+	private Plugin plugin = Mainclass.getPlugin(Mainclass.class);
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
 	{
+		if(sender instanceof Player) {
+			
+		
+		
 		Player p = (Player) sender;
 		
 		if(args.length == 0) {
@@ -20,23 +33,55 @@ public class TpaAccept implements CommandExecutor{
 			
 			Player p2 = Mainclass.tpa.get(p);
 			
-			p2.teleport(p.getLocation());
+			p2.sendMessage(Mainclass.prefix+ "§aTeleportation erfolgt. §cNicht bewegen...");
+			p.sendMessage(Mainclass.prefix + "§aTeleportation erfolgt. §cNicht bewegen...");
+			lastLoc.put(p, p.getLocation());
+			lastLoc2.put(p2, p2.getLocation());
 			
-			p.sendMessage(Mainclass.prefix + "§aTeleportation war erfolgreich!");
+			Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+	               public void run() {
+	            	   Location Playerone =lastLoc.get(p);
+            		   Location Playertwo =lastLoc2.get(p2);
+            		   
+            		   Double distance1 = Playerone.distance(p.getLocation());
+            		   Double distance2 = Playertwo.distance(p2.getLocation());
+            		   
+            		   if((distance1==0.0)&&(distance2==0.0)) {
+            			   try {
+    	            		   
+       	            		
+    	            		   Player p3 = Mainclass.tpa.get(p);
+    		            	   
+    		            	   p3.teleport(p.getLocation());
+    							
+    							p.sendMessage(Mainclass.prefix + "§aTeleportation war erfolgreich!");
+    							
+    							p3.sendMessage(Mainclass.prefix + "§aTeleportation war erfolgreich!");
+    							Mainclass.tpa.remove(p, p2);
+    						
+    					} catch (Exception ex) {
+    						p.sendMessage(Mainclass.prefix + "§aDie Anfrage wurde abgebrochen!");
+    					}
+            			   
+            		   }else {
+            			   p.sendMessage(Mainclass.prefix + "§cEiner von euch hat sich bewegt!");
+            			   p2.sendMessage(Mainclass.prefix + "§cEiner von euch hat sich bewegt!");
+            		   }
+            		   	          	           	   	            	             	   	            	   
+	               }
+	            }, 100L);
 			
-			p2.sendMessage(Mainclass.prefix + "§aTeleportation war erfolgreich!");
 			
-			Mainclass.tpa.remove(p, p2);
 			}catch(Exception ex){
-				p.sendMessage(Mainclass.prefix + "§cDu hast keine Anfrage!");
+				
 			}
 			
 		}
 		else {
-			p.sendMessage(Mainclass.prefix + " §c/tpaccept");
+			p.sendMessage(Mainclass.prefix + " §c/tpaaccept");
 		}
 			
-		
+		}
 		return false;
 	}
 
